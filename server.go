61550eb67a65082119078badb4833ca0878b6130
gchangchen <gchangchen@gmail.com>
2016-03-18 07:21:25 +0800

    test
    1.add speed test, get /speed?size=xxx...

--- server.go
+++ server.go
@@ -4,17 +4,43 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 )
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "\n\tHello World From Golang on Heroku")
+	fmt.Fprintf(w, "\n\tHello World From Golang on Heroku\n\n")
+}
+func speedHandler(w http.ResponseWriter, r *http.Request) {
+	speed_test_len := r.URL.Query().Get("size")
+	n, err:= strconv.Atoi(speed_test_len)
+	if err != nil{
+		n = 10
+	}
+	n *= 1024*1024
+
+	w.Header().Set("Content-Length", strconv.Itoa(n))
+	w.Header().Set("Content-Type", "application/octet-stream")
+	buf := make([]byte, 4096, 4096)
+	for n >= 4096 {
+		_, err := w.Write(buf)
+		if err != nil {
+			break
+		}
+		n -= 4096
+	}
 }
 
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/speed", speedHandler)
 
-	fmt.Println("Server Listening...")
-	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Println("Server Listening on " + port)
+
+	err := http.ListenAndServe(":"+port,  nil)
 	if err != nil {
 		panic("ListenAndServe")
 	}
